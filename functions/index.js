1bb385090f637111237028d1df6474acfad22d52
Bruno Santos <bruno.santos1996@gmail.com>
2018-05-22 17:51:37 +0100

    firebase functions started

--- functions/index.js
+++ functions/index.js
@@ -1,25 +1,26 @@
-/**
- * Copyright 2017 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+'use strict';
 
-// Note: You will edit this file in the follow up codelab about the Cloud Functions for Firebase.
+const functions = require('firebase-functions');
+const TextToSpeechV1 = require('watson-developer-cloud/text-to-speech/v1');
 
-// TODO(DEVELOPER): Import the Cloud Functions for Firebase and the Firebase Admin modules here.
+const textToSpeech = new TextToSpeechV1({
+	username: '5dbdab7a-cc5f-4af4-93ac-235033155bcf',
+	password: 'rI2xbfncQpgP',
+	url: 'https://stream.watsonplatform.net/text-to-speech/api/'
+});
 
-// TODO(DEVELOPER): Write the addWelcomeMessages Function here.
+exports.hello = functions.https.onRequest((req, res) => {
+	return res.status(200).send('hello');	
+});
 
-// TODO(DEVELOPER): Write the blurOffensiveImages Function here.
-
-// TODO(DEVELOPER): Write the sendNotifications Function here.
+exports.tts = functions.https.onRequest((req, res) => {
+	let params = {
+		text: req.query.text,
+		voice: 'en-US-AllisonVoice',
+		accept: 'audio/wav'
+	}
+	
+	textToSpeech.synthesize(params, function(err, audio) {
+		return res.status(200).send(audio);
+	});
+});
